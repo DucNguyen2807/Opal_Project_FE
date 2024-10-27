2d62321ef3fd2f85f9af8cfd1ffd7cb810e872ad
Nguyen Xuan Duc <nguyenxuanduc280703@gmail.com>
2024-10-27 07:42:12 +0700

    [fix]

--- lib/ui/HomePage/HomePage.dart
+++ lib/ui/HomePage/HomePage.dart
@@ -83,7 +83,7 @@
     }
 
     Future<void> saveDeviceToken(String token) async {
-      final url = Uri.parse('https://10.0.2.2:7203/api/Notification/save_device_token');
+      final url = Uri.parse('https://opal.io.vn/api/Notification/save_device_token');
 
       try {
         final prefs = await SharedPreferences.getInstance();
